d012211bb21dd11bd9b3cc50639fa85d511acb15
imiku3939 <minghaohu39@gmail.com>
2022-04-15 10:30:33 +0800

    init repo

---
+++ src/main/java/com/initial/community/CommunitydemoApplication.java
@@ -0,0 +1,13 @@
+package com.initial.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/initial/community/CommunitydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.initial.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
